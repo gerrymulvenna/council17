96d4d62ea18f5799ba40a66a15efd3468136eab9
Gerry Mulvenna <git@movingwifi.com>
2017-04-14 23:40:45 +0100

    The remaining 18 councils added Signed-off-by: Gerry Mulvenna <git@movingwifi.com>

---
+++ councils/midlothian.php
@@ -0,0 +1,22 @@
+<?php
+require $_SERVER["DOCUMENT_ROOT"] . "/website/php/functions.php";
+// --------- council specific variables in this section --------
+// this should be the council identifier consistent with Democracy Club data, this website and to a certain extent the map data
+// used in the mapName js variable, the twitter image, the breadcrumb link
+$slug = 'midlothian';
+$council_name = 'Midlothian';  // used in the title and breadcrumb
+$mapLat = 55.8510;            // good centre position for the map
+$mapLong = -3.1363;          // good centre position for the map 
+$mapZoom = 10;                // zoom level starting position
+$mapProperty = 'Ward_Name';  // the property in the geojson file with the name of the ward
+$mapUnit = 'Ward';           // either Council or Ward
+$mapWardDesc= 'Ward_Code';        // the property in the geojson file with the unique ward identifier
+
+// ------ below here should be the same for each council --------
+
+// function head($title, $mapName, $mapLat, $mapLong, $mapZoom, $mapProperty, $mapUnit, $mapWardDesc, $twimg)
+head("#council17 $council_name - Map-based interface to crowd-sourced data for the Scottish Council elections 2017", $slug, $mapLat, $mapLong, $mapZoom, $mapProperty, $mapUnit, $mapWardDesc, "/website/image/$slug.png");
+navigation("Scottish Council elections 2017");
+content($council_name, $slug);
+foot();
+?>
---
+++ councils/south-lanarkshire.php
@@ -0,0 +1,22 @@
+<?php
+require $_SERVER["DOCUMENT_ROOT"] . "/website/php/functions.php";
+// --------- council specific variables in this section --------
+// this should be the council identifier consistent with Democracy Club data, this website and to a certain extent the map data
+// used in the mapName js variable, the twitter image, the breadcrumb link
+$slug = 'south-lanarkshire';
+$council_name = 'South Lanarkshire';  // used in the title and breadcrumb
+$mapLat = 55.6;            // good centre position for the map
+$mapLong = -3.7821;          // good centre position for the map 
+$mapZoom = 9;                // zoom level starting position
+$mapProperty = 'Ward_Name';  // the property in the geojson file with the name of the ward
+$mapUnit = 'Ward';           // either Council or Ward
+$mapWardDesc= 'Ward_Code';        // the property in the geojson file with the unique ward identifier
+
+// ------ below here should be the same for each council --------
+
+// function head($title, $mapName, $mapLat, $mapLong, $mapZoom, $mapProperty, $mapUnit, $mapWardDesc, $twimg)
+head("#council17 $council_name - Map-based interface to crowd-sourced data for the Scottish Council elections 2017", $slug, $mapLat, $mapLong, $mapZoom, $mapProperty, $mapUnit, $mapWardDesc, "/website/image/$slug.png");
+navigation("Scottish Council elections 2017");
+content($council_name, $slug);
+foot();
+?>
