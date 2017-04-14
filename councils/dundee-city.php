9c2ba4986ff411e77e088d7c4625fbf25843068e
Gerry Mulvenna <git@movingwifi.com>
2017-04-14 17:05:56 +0100

    Clacks, D&G, Dundee, east ayrshire, east dunbartonshire, east lothian, east ren, eilean-siar added Signed-off-by: Gerry Mulvenna <git@movingwifi.com>

---
+++ councils/dundee-city.php
@@ -0,0 +1,23 @@
+<?php
+require $_SERVER["DOCUMENT_ROOT"] . "/website/php/functions.php";
+// --------- council specific variables in this section --------
+
+// this should be the council identifier consistent with Democracy Club data, this website and to a certain extent the map data
+// used in the mapName js variable, the twitter image, the breadcrumb link
+$slug = 'dundee-city';
+$council_name = 'Dundee City';  // used in the title and breadcrumb
+$mapLat = 56.4620;              // good centre position for the map
+$mapLong = -2.9707;          // good centre position for the map 
+$mapZoom = 11;                // zoom level starting position
+$mapProperty = 'NAME';  // the property in the geojson file with the name of the ward
+$mapUnit = 'Ward';           // either Council or Ward
+$mapWardDesc= 'CODE';        // the property in the geojson file with the unique ward identifier
+
+// ------ below here should be the same for each council --------
+
+// function head($title, $mapName, $mapLat, $mapLong, $mapZoom, $mapProperty, $mapUnit, $mapWardDesc, $twimg)
+head("#council17 $council_name - Map-based interface to crowd-sourced data for the Scottish Council elections 2017", $slug, $mapLat, $mapLong, $mapZoom, $mapProperty, $mapUnit, $mapWardDesc, "/website/image/$slug.png");
+navigation("Scottish Council elections 2017");
+content($council_name, $slug);
+foot();
+?>
