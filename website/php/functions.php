db7aae00645cb5bc0fac4c3a9ca8ee37e60ef513
Gerry Mulvenna <git@movingwifi.com>
2017-04-16 01:36:47 +0100

    Google analytics Signed-off-by: Gerry Mulvenna <git@movingwifi.com>

--- website/php/functions.php
+++ website/php/functions.php
@@ -97,9 +97,19 @@ echo '
 	</div>
 	<script src="/website/js/script.js"></script>
 	<script src="/website/js/map.js"></script>
+';
+echo"<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-12076032-17', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </body>
-</html>';
+</html>";
 }
 
 ?>
